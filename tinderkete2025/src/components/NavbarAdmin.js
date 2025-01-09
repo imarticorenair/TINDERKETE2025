f7d30ea5fc8b594a638b2f6b5bcca058c60143b8
lruizsa <lruizsa23wg@ikzubirimanteo.com>
2025-01-09 07:18:05 +0000

    localstorage

--- tinderkete2025/src/components/NavbarAdmin.js
+++ tinderkete2025/src/components/NavbarAdmin.js
@@ -20,7 +20,7 @@ function Navbar() {
   const getActiveClass = (path) => {
     return location.pathname === path ? 'bg-amber-500 rounded-md text-white' : 'text-gray-300'; //Dagoen orrialdeko itxura
   };
-  const isAdmin = localStorage.getItem('isAdmin') === 'true'; // Erabiltzailea admin baldin bada funtzionalitate bat geihago dago
+  //const isAdmin = localStorage.getItem('isAdmin') === 'true'; // Erabiltzailea admin baldin bada funtzionalitate bat geihago dago
   const email = localStorage.getItem('email'); // Emaila berifikatzen du
 
   const toggleMenu = () => {
@@ -38,7 +38,8 @@ function Navbar() {
   const handleLogout = (e) => {
     e.preventDefault();
     localStorage.removeItem('email');
-    localStorage.removeItem('isAdmin');
+    //localStorage.removeItem('isAdmin');
+    localStorage.removeItem("user");
     navigate('/login'); // Hasierako horria
   };
 
